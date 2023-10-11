e7213bb6357bb93c71dd93af3b908e68d08efee6
vishnubharadwaj <visnujai@gmail.com>
2023-10-12 01:28:37 +0530

    Conversion Of Datatypes

---
+++ 03_conversionOperator.js
@@ -0,0 +1,20 @@
+let score = "33aa"
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)  //It gives output as NaN 
+
+//"33"=>33
+//"33aa"=>NaN
+//true => 1: false =>0
+let Numbers = 99
+let stringNumber = String(Numbers)
+
+console.log(typeof Numbers)
+console.log(typeof stringNumber);
+
+
