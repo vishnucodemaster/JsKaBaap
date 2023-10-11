64657a35afaf2203a3afe8b05259568b240a7482
vishnubharadwaj <visnujai@gmail.com>
2023-10-12 01:00:41 +0530

    Datatypes and its outputs

---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert("hello") //Gives a error ,we are using node.js not browser
+
+console.log(3+3)
+//Types of Datatypes
+//number => 2 to power 53
+//bigInt
+//string=>""
+//boolean
+//null=>standalone value
+//undefined=>no value assigned
+//symbol=> unique
+
+//object 
+
+console.log(typeof "Vishnu");//String
+console.log(typeof null) //object
+console.log(typeof undefined)//undefined
