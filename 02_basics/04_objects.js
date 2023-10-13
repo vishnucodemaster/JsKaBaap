f1eb811dbb4ec9589f35ae6bee9f2c4dbeebaeac
vishnubharadwaj <visnujai@gmail.com>
2023-10-14 02:02:39 +0530

    Objects and its methods

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+// const tinderUser = new Object() //This is a singleton object
+const tinderUser = {} //This is not a singleton object
+
+tinderUser.id ="123Test"
+tinderUser.name="Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser ={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Vishnu"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+// const obj3 = {obj1 ,obj2}//here target is obj1 and source is obj2
+const obj3 = Object.assign({},obj1,obj2)//here target is {} and source is obj1 , obj2 this much better syntax
+
+const obj4 = {...obj1,...obj2}
+console.log(obj3) 
+console.log(obj4) 
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
