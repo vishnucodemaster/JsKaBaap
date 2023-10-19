e0457c3c30d911bcb0327244c7e99bfc0109c4d5
vishnubharadwaj <visnujai@gmail.com>
2023-10-19 23:51:36 +0530

    Reduce example

--- 05_Iterations/reduce.js
+++ 05_Iterations/reduce.js
@@ -9,3 +9,10 @@ const myTotal = myNums.reduce( (acc,currval)=>{
     return acc + currval
 },0)
 console.log(myTotal);
+
+const numbers = [15.5, 2.3, 1.1, 4.7];
+document.getElementById("demo").innerHTML = numbers.reduce(getSum, 0);
+
+function getSum(total, num) {
+  return total + Math.round(num);
+}
